7280edf9f55b161afbae1c1ba2f052b51e4bf2b1
Christine Lode <christine.lode@gmail.com>
2017-11-05 13:41:34 -0600

    Add .eslintrc.json and correct lint errors on site.js file

--- js/site.js
+++ js/site.js
@@ -8,10 +8,10 @@ $.noConflict();
   });
 
   $('#navigation ol').after('<a id="menu" href="#">&#9776;</a>');
-	$('#menu').click(function(e) {
-	$(this).toggleClass('active');
-  $('#navigation ol').toggleClass('active');
-  e.preventDefault();
-	});
+  $('#menu').click(function(e) {
+    $(this).toggleClass('active');
+    $('#navigation ol').toggleClass('active');
+    e.preventDefault();
+  });
 
 })(jQuery);
