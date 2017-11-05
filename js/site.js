4519f4927582b5b1afff55e79f4c340d484d7464
Christine Lode <christine.lode@gmail.com>
2017-11-05 13:33:40 -0600

    Add jQuery for video and expanded 3-lined menu that toggles navigation links

--- js/site.js
+++ js/site.js
@@ -2,4 +2,16 @@ $.noConflict();
 
 (function($) {
   $('html').removeClass('no-js').addClass('js');
+
+  $('video').on('click', function() {
+    this.paused ? this.play() : this.pause();
+  });
+
+  $('#navigation ol').after('<a id="menu" href="#">&#9776;</a>');
+	$('#menu').click(function(e) {
+	$(this).toggleClass('active');
+  $('#navigation ol').toggleClass('active');
+  e.preventDefault();
+	});
+
 })(jQuery);
