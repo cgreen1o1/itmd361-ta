7db05dab7270a644a7d5a4da2d1b06c31fc69ad5
Christine Lode <christine.lode@gmail.com>
2017-11-12 16:14:47 -0600

    Remove hidden/expanded navigation menu

--- js/site.js
+++ js/site.js
@@ -7,11 +7,4 @@ $.noConflict();
     this.paused ? this.play() : this.pause();
   });
 
-  $('#navigation ol').after('<a id="menu" href="#">&#9776;</a>');
-  $('#menu').click(function(e) {
-    $(this).toggleClass('active');
-    $('#navigation ol').toggleClass('active');
-    e.preventDefault();
-  });
-
 })(jQuery);
