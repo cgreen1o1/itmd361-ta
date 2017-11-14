d0de9675a5888a4c22c375a08d4e84a76088bfe5
Christine Lode <christine.lode@gmail.com>
2017-11-13 22:25:18 -0600

    Fix eslint errors

--- js/site.js
+++ js/site.js
@@ -1,6 +1,19 @@
 $.noConflict();
 
 (function($) {
+  var mainImage = document.getElementById('hired');
+  var imageArray = ['../media/hired-4-3.jpg',
+    '../media/parade-4-3.jpg',
+    '../media/hawks-star-cropped-4-3.jpg',
+    '../media/k9-4-3.jpg'];
+  var imageIndex = 0;
+  var mainImage2 = document.getElementById('pinum');
+  var imageArray2 = ['../media/pi-by-numbers-4-3.jpg',
+    '../media/irrational-pi-43.jpg',
+    '../media/no-place-like-home-4-3.jpg'];
+  var imageIndex2 = 0;
+  var size = window
+
   $('html').removeClass('no-js').addClass('js');
 
   $('video').on('click', function() {
@@ -8,13 +21,6 @@ $.noConflict();
   });
 
   if(responsiveSize('mobile')) {
-    var mainImage = document.getElementById('hired');
-    var imageArray = ['../media/hired-4-3.jpg',
-      '../media/parade-4-3.jpg',
-      '../media/hawks-star-cropped-4-3.jpg',
-      '../media/k9-4-3.jpg'];
-    var imageIndex = 0;
-
     function changeImage() {
       mainImage.setAttribute('src', imageArray[imageIndex]);
       imageIndex++;
@@ -22,12 +28,6 @@ $.noConflict();
         imageIndex = 0;
       }
     } setInterval(changeImage, 2500);
-
-    var mainImage2 = document.getElementById('pinum');
-    var imageArray2 = ['../media/pi-by-numbers-4-3.jpg',
-      '../media/irrational-pi-43.jpg',
-      '../media/no-place-like-home-4-3.jpg'];
-    var imageIndex2 = 0;
 
     function changeImage2() {
       mainImage2.setAttribute('src', imageArray2[imageIndex2]);
@@ -39,7 +39,6 @@ $.noConflict();
   }
 
   function responsiveSize(target) {
-    var size = window
       .getComputedStyle(document.body, ':after')
       .getPropertyValue('content');
     if(size.indexOf(target) === -1) {
